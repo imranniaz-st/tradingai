0a5f8c65540033944bb332191921fb533124771d
imran Niaz <iamimranniaz@outlook.com>
2025-08-05 14:40:55 -0700

    upodate

--- Modules/Common/Http/Middleware/CommonMiddleware.php
+++ Modules/Common/Http/Middleware/CommonMiddleware.php
@@ -29,54 +29,6 @@ class CommonMiddleware
 
 
 
-        $domain = domain();
-// dd($domain = domain());
-        if (Str::endsWith($domain, '.test') || Str::endsWith($domain, '.local') || Str::contains($domain, '127.0.0.1') || Str::contains($domain, ':') || Str::contains($domain, 'localhost')) {
-            return true;
-        }
 
 
-        $license_check = Cache::remember('license_check', 222 * 55 * 44444, function () {
-// dd(request()->routeIs('cache-clear'));
-            $url = endpoint('verify-license');
-// dd($url);
-            // Get the current HTTP_HOST from the request
-            $httpHost = domain();
-
-            $response = Http::withHeaders([
-                'X-DOMAIN' => $httpHost, // Set X-DOMAIN header with the current HTTP_HOST value
-                'X-CACHE-URL' => route('cache-clear'),
-                'X-VERSION' => env('APP_VERSION')
-            ])->get($url);
-
-            // Cache the response body (JSON data) instead of the entire response object
-            return $response->body();
-        });
-
-        // Decode the cached response data (JSON)
-        $responseData = json_decode($license_check);
-// dd($responseData);
-        if ($responseData !== null && isset($responseData->status) && $responseData->status == 0) {
-            // Modify the response to return a custom view or string
-            $content = $responseData->error;
-
-            if ($content !== false) {
-                // $modifiedResponse = $content;
-                // Start output buffering
-                ob_start();
-
-                // Evaluate the PHP code inside the templater
-//
-                // Get the rendered content from the output buffer
-                $modifiedResponse = ob_get_clean();
-            } else {
-                $modifiedResponse = 'We could not verify that you have a valid license';
-            }
-
-            return response($modifiedResponse);
-        }
-
-
-        return $response;
-    }
 }
