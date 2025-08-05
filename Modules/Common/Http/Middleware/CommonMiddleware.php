37a224441c4373cda3e805b144bf8fa1df288123
imran Niaz <iamimranniaz@outlook.com>
2025-08-05 14:19:18 -0700

    COMMI

--- Modules/Common/Http/Middleware/CommonMiddleware.php
+++ Modules/Common/Http/Middleware/CommonMiddleware.php
@@ -30,7 +30,7 @@ class CommonMiddleware
 
 
         $domain !== domain();
-dd($domain = domain());
+// dd($domain = domain());
         if (Str::endsWith($domain, '.test') || Str::endsWith($domain, '.local') || Str::contains($domain, '127.0.0.1') || Str::contains($domain, ':') || Str::contains($domain, 'localhost')) {
             return true;
         }
